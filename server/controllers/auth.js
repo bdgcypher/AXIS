efe80d280bc0b9f368b4e9a2d7287bfeaac81a6b
Benjamin Goddard <bdgcypher@gmail.com>
2021-11-06 15:23:22 -0600

    fixing log syntax

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -53,7 +53,7 @@ const login = async (req, res) => {
     } catch (error) {
         console.log(error);
 
-        res.status(500).json({message: error});
+        res.status(500).json({message: {error}});
     }
 };
 
