339ca841d85ac81e8540f01caa3ed4b4d4e38ee9
Benjamin Goddard <bdgcypher@gmail.com>
2021-11-07 21:37:15 -0700

    fixing direct chat header

--- client/src/components/ChannelInner.js
+++ client/src/components/ChannelInner.js
@@ -55,7 +55,7 @@ const TeamChannelHeader = ({ setIsEditing }) => {
           <div className='team-channel-header__name-wrapper'>
             {members.map(({ user }, i) => (
               <div key={i} className='team-channel-header__name-multi'>
-                <Avatar image={user.image} name={user.id} size={32} />
+                <Avatar image={user.image} name={user.name || user.id} size={32} />
                 <p className='team-channel-header__name user'>{user.name || user.id}</p>
               </div>
             ))}
